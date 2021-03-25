60fc75d14681bf497b0c093ac371ee0f7e2b2232
Simplation <simplationwang@gmail.com>
2021-03-25 09:43:43 +0800

    fix workflows check failure

--- app/src/main/java/com/example/androiddevchallenge/ui/CountdownTimer.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/CountdownTimer.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import android.os.SystemClock
@@ -7,10 +22,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -70,5 +85,5 @@ fun CountdownTimer(onDarkThemeToggled: () -> Unit, modifier: Modifier = Modifier
 @Preview
 @Composable
 fun PreviewCountdownTimer() {
-    CountdownTimer(onDarkThemeToggled = {  })
+    CountdownTimer(onDarkThemeToggled = { })
 }
--- app/src/main/java/com/example/androiddevchallenge/ui/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/Home.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.BorderStroke
@@ -6,21 +21,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Card
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,8 +43,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
 fun Home() {
@@ -42,21 +57,23 @@ fun Home() {
 
 @Composable
 fun MyApp(onDarkThemeToggled: () -> Unit) {
-    Scaffold(topBar = {
-        TopAppBar(
-            title = {
-                Text(text = stringResource(id = R.string.app_title))
-            },
-            actions = {
-                IconButton(onClick = onDarkThemeToggled) {
-                    Icon(
-                        imageVector = if (MaterialTheme.colors.isLight) Icons.Default.DarkMode else Icons.Default.LightMode,
-                        contentDescription = "Dark theme button"
-                    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_title))
+                },
+                actions = {
+                    IconButton(onClick = onDarkThemeToggled) {
+                        Icon(
+                            imageVector = if (MaterialTheme.colors.isLight) Icons.Default.DarkMode else Icons.Default.LightMode,
+                            contentDescription = "Dark theme button"
+                        )
+                    }
                 }
-            }
-        )
-    }) {
+            )
+        }
+    ) {
         Surface { // color = MaterialTheme.colors.background
             Card(
                 modifier = Modifier
